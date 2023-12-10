40335024e64fc7c90d436dfd27b083c19980b054
alexander <vertiporogov.alexandr@mail.ru>
2023-12-10 15:28:30 +0300

    dddf

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-from utils import get_you_tube_data, create_datebase, save_date_to_datebase
+from utils import get_you_tube_data, create_database, save_date_to_datebase
 from config import config
 
 
 def main():
     api_key = 'AIzaSyARTbNiiO1YNuLkmV_exygBJoEzBJ79T3A'
     channel_ids = [
-        'UC-OVMPlMA3-YCIeg4z5z23A',  # вДудь
-        # 'UCwHL6WHUarjGfUM_586me8w'  # Редакция
+        # 'UC-OVMPlMA3-YCIeg4z5z23A',  # вДудь
+        'UCwHL6WHUarjGfUM_586me8w'  # Редакция
     ]
     params = config()
 
     data = get_you_tube_data(api_key, channel_ids)
-    create_datebase('YouTube', params)
-    # save_date_to_datebase(data, 'YouTube', params)
+    create_database('YouTube', params)
+    # save_date_to_database(data, 'YouTube', params)
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -30,49 +30,79 @@ def get_you_tube_data(api_key: str, channel_ids: [str]) -> list[dict[str, Any]]:
 
     return data
 
-def create_datebase(datebase_name: str, params: dict) -> None:
+def create_database(database_name: str, params: dict) -> None:
     """Создание базы данных и таблиц для сохранения информации о канале и видео"""
 
-    def create_database(database_name: str, params: dict):
-        """Создание базы данных и таблиц для сохранения данных о каналах и видео."""
 
-        conn = psycopg2.connect(dbname='postgres', **params)
-        conn.autocommit = True
-        cur = conn.cursor()
 
-        cur.execute(f"DROP DATABASE {database_name}")
-        cur.execute(f"CREATE DATABASE {database_name}")
+    conn = psycopg2.connect(dbname='postgres', **params)
+    conn.autocommit = True
+    cur = conn.cursor()
 
-        conn.close()
+    cur.execute(f"DROP DATABASE {database_name}")
+    cur.execute(f"CREATE DATABASE {database_name}")
 
-        conn = psycopg2.connect(dbname=database_name, **params)
+    conn.close()
 
-        with conn.cursor() as cur:
-            cur.execute("""
-                CREATE TABLE channels (
-                    channel_id SERIAL PRIMARY KEY,
-                    title VARCHAR(255) NOT NULL,
-                    views INTEGER,
-                    subscribers INTEGER,
-                    videos INTEGER,
-                    channel_url TEXT
+    conn = psycopg2.connect(dbname=database_name, **params)
+
+    with conn.cursor() as cur:
+        cur.execute("""
+            CREATE TABLE channels (
+                channel_id SERIAL PRIMARY KEY,
+                title VARCHAR(255) NOT NULL,
+                views INTEGER,
+                subscribers INTEGER,
+                videos INTEGER,
+                channel_url TEXT
+            )
+        """)
+
+    with conn.cursor() as cur:
+        cur.execute("""
+            CREATE TABLE videos (
+                video_id SERIAL PRIMARY KEY,
+                channel_id INT REFERENCES channels(channel_id),
+                title VARCHAR NOT NULL,
+                publish_date DATE,
+                video_url TEXT
+            )
+        """)
+
+    conn.commit()
+    conn.close()
+
+
+def save_data_to_database(data: list[dict[str, Any]], database_name: str, params: dict):
+    """Сохранение данных о каналах и видео в базу данных."""
+
+    conn = psycopg2.connect(dbname=database_name, **params)
+
+    with conn.cursor() as cur:
+        for channel in data:
+            channel_data = channel['channel']['snippet']
+            channel_stats = channel['channel']['statistics']
+            cur.execute(
+                """
+                INSERT INTO channels (title, views, subscribers, videos, channel_url)
+                VALUES (%s, %s, %s, %s, %s)
+                RETURNING channel_id
+                """,
+                (channel_data['title'], channel_stats['viewCount'], channel_stats['subscriberCount'],
+                 channel_stats['videoCount'], f"https://www.youtube.com/channel/{channel['channel']['id']}")
+            )
+            channel_id = cur.fetchone()[0]
+            videos_data = channel['videos']
+            for video in videos_data:
+                video_data = video['snippet']
+                cur.execute(
+                    """
+                    INSERT INTO videos (channel_id, title, publish_date, video_url)
+                    VALUES (%s, %s, %s, %s)
+                    """,
+                    (channel_id, video_data['title'], video_data['publishedAt'],
+                     f"https://www.youtube.com/watch?v={video['id']['videoId']}")
                 )
-            """)
 
-        with conn.cursor() as cur:
-            cur.execute("""
-                CREATE TABLE videos (
-                    video_id SERIAL PRIMARY KEY,
-                    channel_id INT REFERENCES channels(channel_id),
-                    title VARCHAR NOT NULL,
-                    publish_date DATE,
-                    video_url TEXT
-                )
-            """)
-
-        conn.commit()
-        conn.close()
-
-
-def save_date_to_datebase(data: list[dict[str, Any]], datebase_name: str, params: dict) -> None:
-    """Сохранение данных о каналах и видео в базу данных"""
+    conn.commit()
+    conn.close()
